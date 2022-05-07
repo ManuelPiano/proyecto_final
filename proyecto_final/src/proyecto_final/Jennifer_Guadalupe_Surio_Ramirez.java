6b52d0ec136c6fb21cd74abfbd4e4ea373905d34
jennisurio <101201120+jennisurio@users.noreply.github.com>
2022-05-06 20:53:04 -0600

    Add files via upload

---
+++ proyecto_final/src/proyecto_final/Jennifer_Guadalupe_Surio_Ramirez.java
@@ -0,0 +1,35 @@
+
+package proyecto_final;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
+
+public class Jennifer_Guadalupe_Surio_Ramirez {
+    
+/*
+ 5-. Un trabajador de la industria de la construcción gana $10 diarios en su jornada normal de trabajo. En ciertas ocasiones 
+el empleado tiene que trabajar horas extras para cumplir con su trabajo. En el mes a acumulado 50 horas extras que se la 
+pagan al 10% del valor del pago diario. ¿Cuánto ganara el trabajador al final de un mes de trabajo?
+ */
+    public static void main(String[] args) {
+          NumberFormat formato; formato = new DecimalFormat ("#0.00");
+     
+        double Salariodiario, horasExtra, salarioNeto,salariomensual,salarioFinal,renta,PagoHoraExtra;
+        
+        Salariodiario= 10;
+        horasExtra=50;
+       
+      
+      salarioNeto = 10*30 ;
+       PagoHoraExtra= horasExtra*1; 
+       salariomensual = salarioNeto + PagoHoraExtra;
+      renta= salariomensual*0.10;
+      salarioFinal= salariomensual - renta;
+      
+      System.out.println("El trabajador ganara : $" + formato.format(salarioFinal)+ " " + "al final del mes");
+        
+       
+    }
+}
+
